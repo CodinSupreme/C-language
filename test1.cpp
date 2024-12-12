37baa6f54c7844009901d781d409b626c17fe859
CodinSupreme <blairkimanik@gmail.com>
2024-12-12 07:47:11 +0300

    dd

--- test1.cpp
+++ test1.cpp
@@ -1,27 +1,101 @@
 #include <iostream>
 using namespace std;
 
-class Book{
+class part{
     public:
-    string Author;
-    string Name;
-    int Page;
-
-    Book(string author, string name, int page){
-        Author = author;
-        Name = name;
-        Page = page;
-    }
-
-    void Print(){
-        cout << "Name: " << Name <<endl;
-        cout << "Author: " << Author <<endl;
-        cout << "Page: " << Page <<endl;
+    void Data(){
+        cout << "this is just mulple inheritance test" << endl;
     }
 };
 
+
+class vehicle{
+    protected:
+    string Model_no;
+    float Max;
+
+    public:
+    string Name;
+    int Wheels;
+    int Seats;
+    string Family;
+    string Engine;
+
+    void Max_speed(){
+        cout << Max << endl;
+    }
+
+    void print(){
+        cout << "Name:   " << Name << endl;
+        cout << "Family: " << Family << endl;
+        cout << "Engine: " << Engine << endl;
+        cout << "Seats:  " << Seats << endl;
+        cout << "Wheels: " << Wheels << endl;
+    }
+};
+
+class Truck: public vehicle{
+    public:
+
+    Truck(string name, int typ, int seat, string engine){
+        Name = name;
+        Seats = seat;
+        Engine = engine;
+
+        switch (typ)
+        {
+        case 1:
+            Family = "Trailer";
+            Wheels = 14;
+            Max = 89.2;
+            Model_no = "7RUK102";
+            break;
+        case 2:
+            Family = "Mini-lorry";
+            Wheels = 4;
+            Max = 110.2;
+            Model_no = "7RUK103";
+            break;
+        
+        default:
+            Family = "Lorry";
+            Wheels = 6;
+            Max = 100.0;
+            Model_no = "7RUK100";
+            break;
+        }
+    }
+
+};
+
+class Car: public vehicle, public part{
+    public:
+    Car(string name, string typ, int seat, string engine){
+        Name = name;
+        Wheels = 4;
+        Family = typ;
+        Seats = seat;
+        Engine = engine;
+        Model_no = "C4R1204";
+        friend void Model(Car Vehicle);
+    }
+
+};
+
+void Model(Car Vehicle){
+    cout << "Model: " << Vehicle.Model_no << endl;
+}
+
+
 int main(){
-    Book book1("Blair", "The King", 84);
-    book1.Print();
+    Car car1("Volks Wagon", "Family", 4, "v6");
+    Truck truck1("Isuzu", 3, 3, "V8");
+    Truck truck2("Scania", 1, 3, "V12");
+    Truck truck3("Volks Wagon", 2, 3, "V8");
+
+    car1.Data();
+    car1.print();
+    Model(car1);
+
     return 0;
 }
--- test2.cpp
+++ test2.cpp
@@ -4,61 +4,21 @@
 
 using namespace std;
 
-class Vehicle{
-    public:
-    string manufacturer;
-    string date;
-    string color;
-
-    void Detail(string Color, string Manufacturer, string Date){
-        manufacturer = Manufacturer;
-        date = Date;
-        color = Color;
-    }
-};
-
-class Personal_car:public  Vehicle{
-    private:
-    string number_plate = "";
-
-    void plate_gen(){
-        for (int i=0; i < 6; i++){
-            number_plate += char(rand() % 101);
-        };
-
-    }
-    
-    public:
-    string name;
-    int seats;
-    string steering_side;
-    
-
-    Personal_car(string Name, int Seats, string steer, string Color,string Manufacturer, string Date){
-        name = Name;
-        manufacturer = Manufacturer;
-        date = Date;
-        color = Color;
-        seats = Seats;
-        steering_side = steer;
-        plate_gen();
-
-    }
-
-    void Detail(){
-        cout << "Name:           " <<  name << endl;
-        cout << "Color:          " <<  color << endl;
-        cout << "Seats:          " <<  seats << endl;
-        cout << "Steering Side:  " << steering_side  << endl;
-        cout << "Number Plate:   " <<  number_plate << endl;
-        cout << "Manufacturer:   " <<  manufacturer << endl;
-        cout << "Date:           " <<  date << endl;
-    }
-
-};
-
 int main(){
-    Personal_car Voxwagon("Voxwagon Beetle", 4, "left", "Red", "Voxwagon", "2024-10-26");
-    Voxwagon.Detail();
+    string data = "thought";
+
+    switch(1){
+    case 1:
+        cout << "thought" << endl;
+        break;
+    
+    case 2:
+        cout << "wifi"<< endl;
+        break;
+
+    default:
+        cout << "speed" << endl;
+        break;}
+
     return 0;
 }
